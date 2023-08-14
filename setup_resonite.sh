d9f0664171cc7167c7900a404cb4905b59a57c26
RaithSphere <raithsphere@gmail.com>
2023-08-14 15:50:24 +0100

    Update the setup the create the missing folder just to cover all bases Remove the requirement on the repo

--- setup_resonite.sh
+++ setup_resonite.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 bash "${STEAMCMDDIR}/steamcmd.sh" \
-	+force_install_dir ${STEAMAPPDIR} \
-	+login ${STEAMLOGIN} \
-	+app_license_request ${STEAMAPPID} \
-	+app_update ${STEAMAPPID} -beta ${STEAMBETA} validate \
-	+quit
+    +force_install_dir ${STEAMAPPDIR} \
+    +login ${STEAMLOGIN} \
+    +app_license_request ${STEAMAPPID} \
+    +app_update ${STEAMAPPID} -beta ${STEAMBETA} validate \
+    +quit
 
 if [ "$CLEANASSETS" = true ]; then
-	find ${STEAMAPPDIR}/Headless/Data/Assets -type f -atime +7 -delete
-	find ${STEAMAPPDIR}/Headless/Data/Cache -type f -atime +7 -delete
+    find ${STEAMAPPDIR}/Headless/Data/Assets -type f -atime +7 -delete
+    find ${STEAMAPPDIR}/Headless/Data/Cache -type f -atime +7 -delete
 fi
 if [ "$CLEANLOGS" = true ]; then
-	find /Logs -type f -name *.log -atime +30 -delete
+    find /Logs -type f -name *.log -atime +30 -delete
 fi
+
+mkdir ${STEAMAPPDIR}/Headless/RuntimeData && chown -R resonite:resonite ${STEAMAPPDIR}/Headless/RuntimeData
 
 exec $*
